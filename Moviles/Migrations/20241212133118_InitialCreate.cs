9ce44812d0ca445e2f4c1604d941cc81f0ab57c5
Emilio Tamayo <emilio.tamayo@ayto-caceres.es>
2024-12-12 14:34:10 +0100

    cambio

---
+++ Moviles/Migrations/20241212133118_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Moviles.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Moviles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Marca = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Precio = table.Column<decimal>(type: "DECIMAL(18, 2)", nullable: false),
+                    Capacidad = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moviles", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Moviles");
+        }
+    }
+}
--- Moviles/movil.cs
+++ Moviles/movil.cs
@@ -2,15 +2,16 @@
 {
     public class Movil
     {
+        public int Id { get; set; } // Clave primaria
+        public string Marca { get; set; }
+        public decimal Precio { get; set; }
+        public int Capacidad { get; set; } // Capacidad en GB
+
         public Movil(string marca, decimal precio, int capacidad)
         {
             Marca = marca;
             Precio = precio;
             Capacidad = capacidad;
         }
-
-        public string Marca { get; set; }
-        public decimal Precio { get; set; }
-        public int Capacidad { get; set; } // Capacidad en GB
     }
 }
